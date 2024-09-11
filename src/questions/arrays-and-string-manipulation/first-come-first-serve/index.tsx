75c31da110c0c13d0cac2bce578260a04c602133
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-09-10 23:25:38 -0400

    Add: first come first serve question

---
+++ src/questions/arrays-and-string-manipulation/first-come-first-serve/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function FirstComeFirstServeExample() {
+	const takeOutOrders = [1, 3, 5, 7, 9];
+	const dineInOrders = [2, 4, 6, 8, 10];
+	const servedOrders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	function removeFirstOrder(orders: number[]) {
+		return orders.slice(1, orders.length);
+	}
+
+	function isFirstComeFirstServe(
+		takeOutOrders: number[],
+		dineInOrders: number[],
+		servedOrders: number[],
+	) {
+		let dineInIndex = 0;
+		let takeOutIndex = 0;
+
+		for (const order of servedOrders) {
+			if (
+				takeOutIndex < takeOutOrders.length &&
+				order === takeOutOrders[takeOutIndex]
+			) {
+				takeOutIndex++;
+			} else if (
+				dineInIndex < dineInOrders.length &&
+				order === dineInOrders[dineInIndex]
+			) {
+				dineInIndex++;
+			} else {
+				return false;
+			}
+		}
+
+		if (
+			takeOutIndex !== takeOutOrders.length ||
+			dineInIndex !== dineInOrders.length
+		) {
+			return false;
+		}
+		return true;
+	}
+	return (
+		<div>
+			{isFirstComeFirstServe(takeOutOrders, dineInOrders, servedOrders)}
+		</div>
+	);
+}
