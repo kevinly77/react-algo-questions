a5b1de26713a2d73cf18b682ed4729794d2e9e2d
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-09-12 20:41:57 -0400

    Add: can watch two movies question

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import FirstComeFirstServeExample from "./questions/arrays-and-string-manipulation/first-come-first-serve";
-import MergeSortedArrayExample from "./questions/arrays-and-string-manipulation/merge-sorted-arrays";
-import ReverseStringInPlaceExamples from "./questions/arrays-and-string-manipulation/reverse-string-in-place";
-import ReverseWordsExample from "./questions/arrays-and-string-manipulation/reverse-words";
+import CanWatchAllMoviesExample from "./questions/hashing-hashmaps/can-watch-all-movies";
+
 //TODO separate things into pages and sections
 function App() {
 	return (
 		<div>
-			<FirstComeFirstServeExample />
+			<CanWatchAllMoviesExample />
 		</div>
 	);
 }
