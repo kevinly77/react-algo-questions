a18cf28d296639ac1c89d737b15dee9dc4d1778c
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-09-19 20:14:00 -0400

    Add: max potential profits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import MaxPotentialProfitExample from "./questions/greedy-algorithms/max-potential-profit";
 import CanWatchAllMoviesExample from "./questions/hashing-hashmaps/can-watch-all-movies";
 
 //TODO separate things into pages and sections
 function App() {
 	return (
 		<div>
-			<CanWatchAllMoviesExample />
+			<MaxPotentialProfitExample />
 		</div>
 	);
 }
